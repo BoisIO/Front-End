87fc0cf8a561652070b0e4d31efc2c5d28e0c32a
survivorbat <djbatcat@gmail.com>
2018-06-07 12:43:53 +0200

    Added axios and axios cache instances, also added default router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
+
 import './App.css'
 
 import Header from './ui/components/Header'
 import Footer from './ui/components/Footer'
+
+import axios from 'axios'
+import { throttleAdapterEnhancer, cacheAdapterEnhancer, Cache } from 'axios-extensions'
+
+const http = axios.create({
+	baseURL: '/',
+  headers: { 
+    'Cache-Control': 'no-cache'
+  },
+  adapter: throttleAdapterEnhancer(cacheAdapterEnhancer(axios.defaults.adapter, {enabledByDefault:true, defaultCache: new Cache({maxAge: 1000*60*60})})), //require, basic adapter
+})
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header text="Headertekst"/>
-
+        <Switch>
+          <Route path="/" component={()=> <div axios={http}/>}/>
+        </Switch>
         <Footer text="Footertekst"/>
       </div>
     )
