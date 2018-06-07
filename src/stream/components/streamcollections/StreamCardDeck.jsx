75c12967c095b2dfdab6e7a905ed007c87fa3fa9
Nadermane <timoviveen@live.nl>
2018-06-07 15:05:31 +0200

    Use react-materialize.

--- src/stream/components/streamcollections/StreamCardDeck.jsx
+++ src/stream/components/streamcollections/StreamCardDeck.jsx
@@ -1,17 +1,18 @@
 import React, {Component} from 'react'
 import StreamThumbnail from '../streamthumbnails/StreamThumbnail';
 import {connect} from 'react-redux';
+import {Card, Row, Col} from 'react-materialize'
 
 class StreamCardDeck extends Component {
     render() {
         return (
-            <div className="row">
-                    {this.props.streams.streams.map(item =>
-                    <div key={item.ID} className="col s2"> 
+            <Row>
+                {this.props.streams.streams.map(item =>
+                    <Col s={2} key={item.ID}> 
                         <StreamThumbnail title={item.title} imagesource={item.imagesource} imagealt={item.imagealt}/>
-                    </div>
-                    )}
-            </div>
+                    </Col>
+                )}
+            </Row>
         )
     }
 }
