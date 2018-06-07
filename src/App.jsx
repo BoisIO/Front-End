2fe7b23d0d11afc853c6bcc88c8942199db5eabc
survivorbat <djbatcat@gmail.com>
2018-06-07 12:20:25 +0200

    Added headertext and footertext as UI components

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import './App.css'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div>
+        <Header text="Headertekst"/>
+
+        <Footer text="Footertekst"/>
       </div>
     )
   }
