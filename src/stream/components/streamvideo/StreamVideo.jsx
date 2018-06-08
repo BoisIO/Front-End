c03a3eee402a512e62799fa145ec89f568c0f34a
survivorbat <djbatcat@gmail.com>
2018-06-08 20:07:18 +0200

    Re-added autoPlay to streams

--- src/stream/components/streamvideo/StreamVideo.jsx
+++ src/stream/components/streamvideo/StreamVideo.jsx
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class StreamContainer extends Component {
     render() {
         return (
-            <video /*autoPlay*/ loop controls>
+            <video autoPlay loop controls>
                 <source src="/assets/video/otterdance.mp4" type="video/mp4"/>
                 <source src="/assets/video/otterdance.webm" type="video/webm"/>
             </video>
