139b659e7df40b9aaf1b5f8a5abe7b0758092cb2
survivorbat <djbatcat@gmail.com>
2018-06-09 22:05:02 +0200

    Added redux extension

--- src/store.js
+++ src/store.js
@@ -1,10 +1,12 @@
-import {applyMiddleware, createStore} from 'redux';
+import {applyMiddleware, createStore, compose} from 'redux';
 
-import logger from 'redux-logger';
 import promise from 'redux-promise-middleware';
 
 import reducer from './reducers';
 
-const middleware = applyMiddleware(promise(), logger);
+const middleware = compose( 
+    applyMiddleware(promise()),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+);
 
 export default createStore(reducer, middleware);
