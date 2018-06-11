2f8527b639fd3bda8549b098877941dd56aca1f9
survivorbat <djbatcat@gmail.com>
2018-06-11 12:01:03 +0200

    Searchbar is now full width

--- src/ui/components/searchbar/SearchBar.jsx
+++ src/ui/components/searchbar/SearchBar.jsx
@@ -6,7 +6,7 @@ import {searchStreams} from '../../../stream/actions/streams'
 class SearchBar extends Component {
     render() {
         return (
-            <Input s={6} m={6} l={6} placeholder="Search an item" onKeyUp={(e) => this.props.dispatch(searchStreams(e.target.value)) } />
+            <Input s={12} m={12} l={12} placeholder="Search an item" onKeyUp={(e) => this.props.dispatch(searchStreams(e.target.value)) } />
         )
     }
 }
