f5a66def898545a3203c8c6ff48f2749270b8edd
Nadermane <timoviveen@live.nl>
2018-06-11 12:52:49 +0200

    Add a bit of login ui

--- src/authentication/components/Login.jsx
+++ src/authentication/components/Login.jsx
@@ -3,7 +3,19 @@ import React, { Component } from 'react'
 class Footer extends Component {
   render() {
     return (
-      <span>login :D</span>
+      <div className="container">
+        <div className="section">
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="user" type="text" class="validate" />
+              <label for="user">User</label>
+            </div>
+            <div class="input-field col s12">
+              <a class="waves-effect waves-light btn-large"><i class="material-icons right">send</i>Log in</a>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- src/ui/components/Header.jsx
+++ src/ui/components/Header.jsx
@@ -6,29 +6,26 @@ class Header extends Component {
   render() {
     return (
       <nav>
-          <div className="nav-wrapper">
-      <Row>
-        <Col l={2} s={0} m={0}/>
-        <Col l={10} s={12} m={12}>
-        
-            <Link id="logo-container" to="/" className="brand-logo vertical-align-center">
-              <img src="/assets/img/favicon-32x32.png" className="vertical-middle gsy-logo" alt=""/>
-              <span className="vertical-middle hide-on-med-and-down">GoStreamYourself</span>
-            </Link>
-            <ul className="right hide-on-med-and-down">
-              <li><Link to="/streams">Navbar Link</Link></li>
-            </ul>
-
-            <ul id="nav-mobile" className="sidenav">
-              <li><Link to="/streams">Navbar Link</Link></li>
-            </ul>
-            <Link to="/streams" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
-          
-        </Col>
-      </Row>
-      </div>
-        </nav>
-    )
+        <div className="nav-wrapper">
+          <Row>
+            <Col l={2} s={0} m={0}/>
+            <Col l={10} s={12} m={12}>
+              <Link id="logo-container" to="/" className="brand-logo vertical-align-center">
+                <img src="/assets/img/favicon-32x32.png" className="vertical-middle gsy-logo" alt=""/>
+                <span className="vertical-middle hide-on-med-and-down">GoStreamYourself</span>
+              </Link>
+              <ul className="right hide-on-med-and-down">
+                <li><Link to="/streams">Navbar Link</Link></li>
+              </ul>
+              <ul id="nav-mobile" className="sidenav">
+                <li><Link to="/streams">Navbar Link</Link></li>
+              </ul>
+              <Link to="/streams" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
+            </Col>
+          </Row>
+        </div>
+      </nav>
+      )
   }
 }
 
