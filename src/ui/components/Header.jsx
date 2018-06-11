f7caab85bf72ab3442815ee779e610ff44f21a77
survivorbat <djbatcat@gmail.com>
2018-06-11 13:35:18 +0200

    Fixed header merge conflcit

--- src/ui/components/Header.jsx
+++ src/ui/components/Header.jsx
@@ -6,31 +6,6 @@ class Header extends Component {
   render() {
     return (
       <nav>
-<<<<<<< HEAD
-          <div className="nav-wrapper">
-      <Row>
-        <Col l={2} s={0} m={0}/>
-        <Col l={10} s={12} m={12}>
-        
-            <Link id="logo-container" to="/" className="brand-logo vertical-align-center">
-              <img src="/assets/img/favicon-32x32.png" className="vertical-middle gsy-logo" alt=""/>
-              <span className="vertical-middle hide-on-med-and-down">GoStreamYourself</span>
-            </Link>
-            <ul className="right hide-on-med-and-down">
-              <li><Link to="/people">All transparent people</Link></li>
-            </ul>
-
-            <ul id="nav-mobile" className="sidenav">
-              <li><Link to="people">All transparent people</Link></li>
-            </ul>
-            <Link to="/streams" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
-          
-        </Col>
-      </Row>
-      </div>
-        </nav>
-    )
-=======
         <div className="nav-wrapper">
           <Row>
             <Col l={2} s={0} m={0}/>
@@ -40,10 +15,10 @@ class Header extends Component {
                 <span className="vertical-middle hide-on-med-and-down">GoStreamYourself</span>
               </Link>
               <ul className="right hide-on-med-and-down">
-                <li><Link to="/streams">Navbar Link</Link></li>
+                <li><Link to="/people">All transparent people</Link></li>
               </ul>
               <ul id="nav-mobile" className="sidenav">
-                <li><Link to="/streams">Navbar Link</Link></li>
+                <li><Link to="/people">All transparent people</Link></li>
               </ul>
               <Link to="/streams" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></Link>
             </Col>
@@ -51,7 +26,6 @@ class Header extends Component {
         </div>
       </nav>
       )
->>>>>>> 1b27357d8bc955b9713e17b30b2649a850bc8f19
   }
 }
 
