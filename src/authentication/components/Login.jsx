8f84137207754bbf91008ef9c314356d817aee78
Nadermane <timoviveen@live.nl>
2018-06-12 11:22:03 +0200

    Clean up login UI

--- src/authentication/components/Login.jsx
+++ src/authentication/components/Login.jsx
@@ -1,50 +1,51 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import crypto from 'crypto'
+import {Row, Col, Input} from 'react-materialize'
 
 class Footer extends Component {
   constructor() {
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
+    super()
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleSubmit(event) {
-    event.preventDefault();
     event.persist()
-    const data = new FormData(event.target);
+    event.preventDefault()
 
-    axios.get('https://back3ndb0is.herokuapp.com/login')
+    axios.get('http://back3ndb0is.herokuapp.com/login')
       .then(function (response) {
+        const token = response.headers.token
         const file = event.target.file.files[0]
-        const token = response.headers.token;
+        const name = event.target.user.value
 
-        var sign = crypto.createSign('RSA-SHA256');
-        sign.write(token);
-        sign.end();
+        if (token && name && file) {
+          let reader = new FileReader()
+          reader.onload = function(e) {
+            let sign = crypto.createSign('RSA-SHA256')
+            sign.write(token)
+            sign.end()
 
-        if (file) {
-          var r = new FileReader();
-          r.onload = function(e) { 
-            var contents = e.target.result;
-            let payload = {
-              signature: sign.sign(contents, 'hex'),
-              token: token,
-              name: "Rowin van Blokland"
+            let contents = e.target.result
+            let headers = {
+              'Signature': sign.sign(contents, 'hex'),
+              'Token': token,
+              'Name': name
             }
 
-            axios.post('https://back3ndb0is.herokuapp.com/', JSON.stringify(payload), {headers: {'Content-Type': 'application/json'}})
-            .then(function (response) {
-              console.log(response);
-            })
+            axios.post('http://back3ndb0is.herokuapp.com/', '', {headers: headers})
+              .then(function (response) {
+                console.log(response)
+              })
           }
-          r.readAsText(file);
+          reader.readAsText(file)
         } else { 
-          alert("Failed to load file");
+          alert("Not all data was entered.")
         }
       })
       .catch(function (error) {
-        console.log(error);
-      });
+        console.log(error)
+      })
   }
 
   render() {
@@ -52,28 +53,27 @@ class Footer extends Component {
       <div className="container">
         <div className="section">
           <form onSubmit={this.handleSubmit.bind(this)}>
-            <div class="row">
-              <div class="input-field col s12">
-                <input id="user" type="text" class="validate" />
-                <label for="user">User</label>
-              </div>
-            </div>
-            <div className="row">
-              <div class="file-field input-field col s12">
-                <div class="btn">
-                  <span>File</span>
+            <Row>
+              <Input name="user" placeholder="Enter your name…" s={12} label="User" validate />
+            </Row>
+            <Row>
+              <div className="file-field input-field col s12">
+                <div className="btn waves-effect waves-light">
+                  <span><i className="material-icons">add</i></span> 
                   <input id="file" type="file" />
                 </div>
-                <div class="file-path-wrapper">
-                  <input class="file-path validate" type="text" />
+                <div className="file-path-wrapper">
+                  <input className="file-path validate" type="text" placeholder="Please select your certificate…" />
                 </div>
               </div>
-            </div>
-            <div className="row">
-              <div class="input-field col s12">
-                <input type="submit" class="waves-effect waves-light btn-large" value="Log in" />
-              </div>
-            </div>
+            </Row>
+            <Row>
+              <Col s={12}>
+                <button className="btn waves-effect waves-light" type="submit" name="action">Submit
+                    <i className="material-icons right">cloud</i>
+                </button>
+              </Col>
+            </Row>
           </form>
         </div>
       </div>
