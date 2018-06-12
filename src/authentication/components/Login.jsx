89dce2bb33ad7e2df4fabc7d03d9e6a68d37db27
Nadermane <timoviveen@live.nl>
2018-06-12 13:25:24 +0200

    Use correct axios import.

--- src/authentication/components/Login.jsx
+++ src/authentication/components/Login.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+import axios from "../../axios";
 import crypto from 'crypto'
 import {Row, Col, Input} from 'react-materialize'
 
