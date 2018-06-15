92a95803a7403f15c290a56197c4367e20dea9f8
survivorbat <mheijden15@avans.nl>
2018-06-15 23:00:05 +0200

    Chat verification is back

--- src/socket.js
+++ src/socket.js
@@ -1,16 +1,21 @@
 import openSocket from 'socket.io-client';
-import {signToken} from './signatures'
+import {signToken, verify} from './signatures'
 
 let socket
 
 function open() {
     socket = openSocket('http://back3ndb0is.herokuapp.com/chat/socket');
+    //socket = openSocket('http://localhost:5000/chat/socket');
 }
 
 function subscribeToChat(cb) {
     open()
     socket.on('MESSAGE', data => {
-      cb(data)
+        if(verify(JSON.stringify(data.Content), data.Signature)) {
+            cb(data)
+        } else {
+            alert('Received dirty data from server!')
+        }
     });
 }
 
