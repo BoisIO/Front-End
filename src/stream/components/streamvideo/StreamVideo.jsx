232c466ec20a3e98f487ade242550994eccedbe8
survivorbat <djbatcat@gmail.com>
2018-06-18 16:53:28 +0200

    Added default source

--- src/stream/components/streamvideo/StreamVideo.jsx
+++ src/stream/components/streamvideo/StreamVideo.jsx
@@ -4,8 +4,7 @@ class StreamContainer extends Component {
     render() {
         return (
             <video autoPlay loop controls style={{backgroundColor: "#263238", color: "white"}}>
-                {/* <source src={this.props.stream.Source} type="video/mp4"/> */}
-                <source src="rtsp://mpv.cdn3.bigCDN.com:554/bigCDN/mp4:bigbuckbunnyiphone_400.mp4" type="application/x-rtsp"/>
+                <source src="/assets/video/otterdance.mp4" type="mp4"/>
                 Your browser does not support the VIDEO tag and/or RTP streams.
             </video>
         )
