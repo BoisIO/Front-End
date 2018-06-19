3406a28dda62ae5d74a3a918466f73cebf0889e2
survivorbat <djbatcat@gmail.com>
2018-06-19 13:02:41 +0200

    Addded something that itmo didnt want to add

--- src/chat/components/chatboxcontainer/ChatBoxContainer.jsx
+++ src/chat/components/chatboxcontainer/ChatBoxContainer.jsx
@@ -1,8 +1,14 @@
 import React, {Component} from 'react'
-import ReactDOM from 'react-dom'
 import ChatMessage from '../../../chat/components/chatmessage/ChatMessage'
+import { Button } from 'react-materialize';
 
 class ChatBoxContainer extends Component {
+    constructor() {
+        super()
+        this.state = {
+            loadHistory: false
+        }
+    }
     scrollToBottom = () => {
         this.messagesEnd.scrollIntoView({ behavior: "smooth" });
     }
@@ -19,7 +25,13 @@ class ChatBoxContainer extends Component {
         return (
             <div>
                 <ul id="chat-messages" className="collection" style={{overflowY: 'scroll', margin: '0', maxHeight: "250px"}}>
-                    {this.props.stream.messages.slice(Math.max(this.props.stream.messages.length - 50, 1)).map((item, key) => <ChatMessage key={key} message={item} />)}
+                    {!this.state.loadHistory?<li className="collection-item center">
+                        <Button onClick={() => this.setState({loadHistory: true})}>Load history</Button>
+                    </li>:null}
+                    {this.state.loadHistory?
+                        this.props.stream.messages.map((item, key) => <ChatMessage key={key} message={item} />)
+                        :
+                        this.props.stream.messages.slice(Math.max(this.props.stream.messages.length - 50, 1)).map((item, key) => <ChatMessage key={key} message={item} />)}
                     <div style={{ float:"left", clear: "both" }}
                         ref={(el) => { this.messagesEnd = el; }}>
                     </div>
--- src/chat/components/chatmessage/ChatMessage.jsx
+++ src/chat/components/chatmessage/ChatMessage.jsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react'
-import { Chip } from 'react-materialize'
 import moment from 'moment'
 import './ChatMessage.css'
 
