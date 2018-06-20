7f80a49622aa16256f6f4edd0f5e9f57edf1caa5
survivorbat <djbatcat@gmail.com>
2018-06-20 12:21:37 +0200

    Works yay

--- src/stream/components/streamvideo/StreamVideo.jsx
+++ src/stream/components/streamvideo/StreamVideo.jsx
@@ -1,30 +1,27 @@
 import React, {Component} from 'react'
+import openSocket from 'socket.io-client'
 
 class StreamVideo extends Component {
     componentDidMount(){
-        const canvas = this.refs.videoplayer
-
-        const socket = window.io('http://145.49.55.236:8001')
-        const self = this
+        const socket = openSocket('http://145.49.55.236:8001')
         socket.on('connect',function(){
             socket.emit('f', {
                 function:'getStream',
-                feed: self.props.stream._id
+                feed: '1'
             })
         })
 
         const player = new window.JSMpeg.Player('pipe',{
-            canvas:canvas
+            canvas: this.refs['videocanvas_'+this.props.stream._id]
         });
 
         socket.on('h264', function (data) {
             player.write(data.buffer)
         });
     }
-    shouldComponentUpdate() { return false }
     render() {
         return (
-            <canvas style={{backgroundColor:"black"}} height="200" width="200" ref="videoplayer"></canvas>
+            <canvas style={{backgroundColor:"black"}} ref={"videocanvas_"+this.props.stream._id}></canvas>
         )
     }
 }
