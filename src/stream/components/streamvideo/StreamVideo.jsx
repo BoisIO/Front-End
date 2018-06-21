8c3d7dfeda1325530da2741fca9c900ef6f0857d
Nadermane <timoviveen@live.nl>
2018-06-21 15:48:12 +0200

    (Sadly) fix the streams using iframes.

--- src/stream/components/streamvideo/StreamVideo.jsx
+++ src/stream/components/streamvideo/StreamVideo.jsx
@@ -3,37 +3,36 @@ import React, {Component} from "react"
 import {JSMpeg} from './jsmpeg'
 
 class StreamVideo extends Component {
-    socket = null
-    player = null
-    componentDidMount(){
-        console.log("http://145.49.53.161:"+this.props.stream.Port+"/")
-        this.socket = openSocket("http://145.49.53.161:"+this.props.stream.Port+"/")
-        //const socket = openSocket("http://145.49.55.236:"+this.props.stream.Port+"/")
-        const self = this
-        this.socket.on("connect",function(){
-            self.socket.emit("f", {
-                function:"getStream"
-            })
-        })
-        this.player = new JSMpeg.Player(this.props.stream._id,{
-            canvas: this.refs["videocanvas_"+this.props.stream._id]
-        })
-
-        this.socket.on("h264", function (data) {
-            console.log("This is stream %s en we're receiving data from %s",self.props.stream.User._id, data.stream)
-            if(data.stream === self.props.stream.User._id) {
-                self.player.write(data.buffer)
-            }
-        })
-    }
-    componentWillUnmount() {
-        this.socket.emit('end', () => {
-            this.player = null
-        })
-    }
+    // socket = null
+    // player = null
+    // componentDidMount(){
+    //     console.log("http://145.49.53.161:"+this.props.stream.Port+"/")
+    //     this.socket = openSocket("http://145.49.53.161:"+this.props.stream.Port+"/")
+    //     //const socket = openSocket("http://145.49.55.236:"+this.props.stream.Port+"/")
+    //     const self = this
+    //     this.socket.on("connect",function(){
+    //         self.socket.emit("f", {
+    //             function:"getStream"
+    //         })
+    //     })
+    //     this.player = new JSMpeg.Player(this.props.stream._id,{
+    //         canvas: this.refs["videocanvas_"+this.props.stream._id]
+    //     })
+    //     this.socket.on("h264", function (data) {
+    //         console.log("This is stream %s en we're receiving data from %s",self.props.stream.User._id, data.stream)
+    //         if(data.stream === self.props.stream.User._id) {
+    //             self.player.write(data.buffer)
+    //         }
+    //     })
+    // }
+    // componentWillUnmount() {
+    //     this.socket.emit('end', () => {
+    //         this.player = null
+    //     })
+    // }
     render() {
         return (
-            <canvas style={{backgroundColor:"black"}} ref={"videocanvas_"+this.props.stream._id}></canvas>
+            <iframe src={"http://145.49.53.161:" + this.props.stream.Port} frameBorder="0" style={{background: "#000"}}></iframe>
         )
     }
 }
